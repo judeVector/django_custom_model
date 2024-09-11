36045bf0c6d965a079f404dd9249706cecb5039b
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-09-11 19:59:57 +0400

    Create a UserAdminConfig to setup the admin interface

--- users/admin.py
+++ users/admin.py
@@ -7,6 +7,7 @@ from .models import NewUser
 # Register your models here.
 class UserAdminConfig(UserAdmin):
 
+    model = NewUser
     list_filter = ["email", "user_name", "first_name", "is_active", "is_staff"]
     search_fields = ["email", "user_name", "first_name"]
     ordering = [
@@ -34,6 +35,27 @@ class UserAdminConfig(UserAdmin):
             },
         ],
     ]
+    formfield_overrides = {
+        NewUser.about: {"widget": Textarea(attrs={"rows": 10, "cols": 40})}
+    }
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "user_name",
+                    "first_name",
+                    "about",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
 
 
 admin.site.register(NewUser, UserAdminConfig)
